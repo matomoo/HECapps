1bbcbc15995df4f8d70f1a8fdd600065301c59bd
zyxgo <me.rahmat.hidayat@gmail.com>
2018-11-19 08:16:56 +0800

    step done - fixing menu

--- src/container/HomeContainer/index.tsx
+++ src/container/HomeContainer/index.tsx
@@ -238,13 +238,13 @@ export default class HomeContainer extends React.Component<Props, State> {
 					<Left><Text>View percentage of share</Text></Left>
 					<Right><Icon active name="ios-arrow-forward"/></Right>
 				</CardItem>
-				<CardItem
+				{/* <CardItem
 					key="4"
 					button
 					onPress={() => this.props.navigation.navigate("ProfileDokterPage")} >
 					<Left><Text>Profil Dokter</Text></Left>
 					<Right><Icon active name="ios-arrow-forward"/></Right>
-				</CardItem>
+				</CardItem> */}
 			</Card>
 		);
 
